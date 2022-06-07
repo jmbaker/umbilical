9556e80a563b019f978f3a6730f122c36c5ac1dd
Jeremy M. Baker <rekabmj@gmail.com>
2022-06-06 21:08:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "terraform-example-queue"
-  delay_seconds             = 90
+  delay_seconds             = 91
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
